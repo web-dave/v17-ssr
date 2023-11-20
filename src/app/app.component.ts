33f57f0a7cd5bc630ecd47cbe7feb0d81a103168
Dave <dave@webdave.de>
2023-11-20 22:10:51 +0100

    stream

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,20 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { AsyncPipe, CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, AsyncPipe],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'v17-ssr';
+  http = inject(HttpClient);
+  foo = 2;
+  data$ = this.http.get<{ [key: string]: any }[]>(
+    'http://localhost:3000/starships'
+  );
 }
